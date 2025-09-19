e926eb6d9379299540f54430edc1de415a773f62
Pere <perebercerotrigell@gmail.com>
2025-09-19 06:27:19 +0200

    update dates.py

--- dates.py
+++ dates.py
@@ -1,44 +1,3 @@
-<<<<<<< HEAD:dates.py
-## This is the first lesson of intermediate python course of Mouredev
-
-#Importaciones 
-from datetime import datetime
-from datetime import date 
-from datetime import time
-
-#1. Crea una variable con la fecha y hora actual.
-
-actual_date = datetime.now()
-print(actual_date)
-#2. Imprime solo el año, mes y día de la fecha actual.
-print(f"The date of today is {actual_date.day} / {actual_date.month} / {actual_date.year } ")
-#3. Crea una fecha específica: 25 de diciembre de 2025 y muéstrala.
-
-specific_date =  date(2025,12,25)
-print(specific_date)
-#4. Muestra solo la hora, los minutos y los segundos de un objeto time.
-
-tiempo = time(6,25,39)
-print(f"Son las {tiempo.hour} y {tiempo.minute} minutos , {tiempo.second} segundos")
-
-# 5. Calcula cuántos días faltan para el 1 de enero del año siguiente.
-enero = date(2026,1,1)
-esto_is_hoy = date.today()
-diferencia_between =  enero - esto_is_hoy
-print(f"Left time for new year : {diferencia_between}")
-#6. Crea una función que reciba una fecha y devuelva su timestamp.
-
-def get_timestamp(date):
-    return date.timestamp()
-
-print(get_timestamp(datetime.now()))
-    
-       
-#7. Suma 30 días a la fecha actual usando timedelta.
-#8. Crea una fecha y añade 1 mes (consejo: hazlo sumando 30 días como simplificación).
-#9. Compara dos fechas y muestra cuál es anterior.
-#10. Crea una lista con varias fechas y ordénalas cronológicamente.
-=======
 ## This is the first lesson of intermediate python course of Mouredev
 
 #Importaciones 
@@ -104,4 +63,3 @@ varias_Fechas.sort()
 print(varias_Fechas)
 
  
->>>>>>> c137de8 (Done_Dates):Intermediate_Py/dates.py
