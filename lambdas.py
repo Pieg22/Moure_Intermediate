2103850e9b41e5690ec4db452b9734a8ae8c21ed
Pere <perebercerotrigell@gmail.com>
2025-09-25 06:54:40 +0200

    add lambdas.py

---
+++ lambdas.py
@@ -0,0 +1,21 @@
+# 1. Crea una lambda que sume dos nÃºmeros.
+sumar = lambda primer_numero , segundo_numero : primer_numero + segundo_numero 
+print(sumar(100,3000))
+
+# 2. Crea una lambda que calcule el cuadrado de un nÃºmero.
+
+# 3. Crea una lambda que devuelva el mayor de dos nÃºmeros.
+
+# 4. Crea una lambda que sume 10 a un nÃºmero dado.
+
+# 5. Crea una lambda que devuelva el Ãºltimo carÃ¡cter de una cadena.
+
+# 6. Crea una lambda que indique si una palabra tiene mÃ¡s de 6 letras.
+
+# 7. Crea una lambda que convierta una cadena a minÃºsculas.
+
+# 8. Crea una lambda que devuelva True si un nÃºmero es positivo.
+
+# 9. Crea una lambda que devuelva "Cadena vacÃ­a" si el string estÃ¡ vacÃ­o.
+
+# 10. Crea una lambda que calcule el precio final con un impuesto aÃ±adido del 21%.
